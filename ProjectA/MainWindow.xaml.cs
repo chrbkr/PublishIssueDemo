ad469b3c7867a4b9be3e3244e82a31a95a227513
Christian Obkircher <chrbkr@users.noreply.github.com>
2021-07-14 21:25:09 +0200

    Create demo project.

---
+++ ProjectA/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ProjectB;
+using System.Windows;
+
+namespace ProjectA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConfigReader configReader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DummyLabel.Content = "Hello";
+        }
+
+        private void ShowEndpoint(object sender, RoutedEventArgs e)
+        {
+            configReader = new ConfigReader();
+            DummyLabel.Content = configReader.GetEndpoint();
+        }
+    }
+}
---
+++ ProjectB/ConfigReader.cs
@@ -0,0 +1,30 @@
+﻿using ProjectB.Properties;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Configuration;
+
+namespace ProjectB
+{
+    public class ConfigReader
+    {
+
+        public string GetEndpoint()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(GetType().Assembly.Location);
+            ClientSection clientSection = (ClientSection)config.GetSection("system.serviceModel/client");
+
+            IEnumerable<ChannelEndpointElement> configEndpoints = clientSection
+                .Endpoints
+                .OfType<ChannelEndpointElement>();
+
+            EndpointAddress endpointAddress = new EndpointAddress(new UriBuilder(configEndpoints
+                .First()
+                .Address).Uri);
+
+            return endpointAddress.ToString();
+        }
+    }
+}
